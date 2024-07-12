5724911f7ba15cfab8c5e562278cf11bbe07dbcc
Rishi Jha <119756861+rishi-jha7@users.noreply.github.com>
2024-07-12 23:46:48 +0530

    Update bubble_sort.js

--- Scripts/bubble_sort.js
+++ Scripts/bubble_sort.js
@@ -19,12 +19,14 @@ function Bubble()
 
     for(var i=0;i<array_size-1;i++)
     {
+       flag=false;
         for(var j=0;j<array_size-i-1;j++)
         {
             div_update(divs[j],div_sizes[j],"yellow");//Color update
 
             if(div_sizes[j]>div_sizes[j+1])
             {
+               flag=true;
                 div_update(divs[j],div_sizes[j], "red");//Color update
                 div_update(divs[j+1],div_sizes[j+1], "red");//Color update
 
@@ -40,6 +42,7 @@ function Bubble()
         div_update(divs[j],div_sizes[j], "green");//Color update
     }
     div_update(divs[0],div_sizes[0], "green");//Color update
+    if(flag==false) break;
 
     enable_buttons();
 }
